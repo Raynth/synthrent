3cf10462ed454f4f6828238b86a2303361365b41
Raymond van Kouwen <r.van.kouwen@gmail.com>
2018-04-19 08:01:58 +0200

    CustomerAccountController@update toegevoegd

--- app/Http/Controllers/User/CustomerAccountController.php
+++ app/Http/Controllers/User/CustomerAccountController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Auth;
 use App\Model\admin\Rental;
 use App\Model\user\User;
+use Illuminate\Support\Facades\Hash;
 
 class CustomerAccountController extends Controller
 {
@@ -30,5 +31,33 @@ class CustomerAccountController extends Controller
         $customer = User::find(Auth::id());
         
         return view('account.edit', compact('customer'));
+    }
+
+    //
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'forename' => 'required',
+            'lastname' => 'required',
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        $customer = User::find($id);
+        $customer->forename = $request->input('forename');
+        $customer->lastname = $request->input('lastname');
+        $customer->email = $request->input('email');
+        $customer->password = Hash::make($request->input('password'));
+        $customer->street = $request->input('street');
+        $customer->number = $request->input('number');
+        $customer->zipcode = $request->input('zipcode');
+        $customer->city = $request->input('city');
+        $customer->phone = $request->input('phone');
+        $customer->account_number = $request->input('account_number');
+        $customer->identification = $request->input('identification');
+        $customer->discount = $request->input('discount');
+        $customer->save();
+
+        return redirect()->route('account.index')->with('success', 'Gegevens bijgewerkt');
     }
 }
