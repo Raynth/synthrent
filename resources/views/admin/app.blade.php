afd2334f3f35bc0752463e7e9001d73842067959
Raymond van Kouwen <r.van.kouwen@gmail.com>
2018-05-19 12:24:36 +0200

    AdminLTE actieve menu aangepast

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -412,18 +412,24 @@
     </script>
     <!-- Active menu -->
     <script>
-        /** add active class and stay opened when selected */
-        var url = window.location;
+        $(document).ready(function(){
 
-        // for sidebar menu entirely but not cover treeview
+        var url = window.location;
+        // Will only work if string in href matches with location
+        $('ul.sidebar-menu a[href="'+ url +'"]').parent().addClass('active');
+
+        // Will also work for relative and absolute hrefs
         $('ul.sidebar-menu a').filter(function() {
             return this.href == url;
         }).parent().addClass('active');
 
-        // for treeview
-        $('ul.treeview-menu a').filter(function() {
-            return this.href == url;
-        }).parentsUntil(".sidebar-menu > .treeview-menu").addClass('active');
+        // Will also work for relative and absolute hrefs
+        $('ul.sidebar-menu a').filter(function() {
+            var regex = new RegExp('\\b' + this.href + '\\b');
+            return url.href.search(regex) !== -1;
+        }).parent().addClass('active');
+
+        });
     </script>
 
     @section('footerSection')
